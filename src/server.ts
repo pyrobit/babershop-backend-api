8ee4a0721eb2a53024d9987ef12d42c495c9b4b5
pyrobit <3159587+pyrobit@users.noreply.github.com>
2020-07-17 00:42:55 -0300

    appointments post done

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import routes from './routes';
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.post('/users',(req,res)=>{
+  const { name, email} = req.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return res.json(user);
+});
+
+app.listen(3333, () =>{
+    console.log("🛫 Server is Up! Using port 3333" );
+});
